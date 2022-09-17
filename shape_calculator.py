3b1197ded3f62ca42be87c7a57b1cfea1b7d24dd
thbob2 <33073278+thbob2@users.noreply.github.com>
2022-09-18 00:19:24 +0100

    test ok

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,15 +1,66 @@
-# This entrypoint file to be used in development. Start by reading README.md
-import shape_calculator
-from unittest import main
-rect = shape_calculator.Rectangle(5, 10)
-print(rect.get_area())
-rect.set_width(3)
-print(rect.get_perimeter())
-print(rect)
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-# Run unit tests automatically
-main(module='test_module', exit=False)
+import math
+
+
+
+class Rectangle(object):
+
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+    
+    def set_width(self,width):
+        self.width = width
+    
+    def set_height(self,height):
+        self.height = height
+    
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return (self.width + self.height) * 2
+    
+    def get_diagonal(self):
+        return math.sqrt(self.width**2 + self.height**2)
+    
+    def get_picture(self):
+        
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        picture = ""
+        for line in range(self.height):
+            picture += "*" * self.width +"\n"
+        
+        return picture
+    
+    def get_amount_inside(self,rectangle):
+        if self.get_area() < rectangle.get_area():
+            return 0
+        
+        return int(self.get_area() / rectangle.get_area())
+        
+    
+    def __str__(self):
+        return "Rectangle(width={}, height={})".format(self.width,self.height)
+    
+    
+    
+    
+class Square(Rectangle):
+    
+    def __init__(self, side):
+        super().__init__(side, side)
+        self.side = side    
+    def set_side(self,side):
+        self.height = side
+        self.width = side
+        self.side = side
+    
+    def set_height(self, height):
+        self.set_side(height)
+    
+    def set_width(self, width):
+        self.set_side(width)
+
+    def __str__(self):
+        return "Square(side={})".format(self.side)
